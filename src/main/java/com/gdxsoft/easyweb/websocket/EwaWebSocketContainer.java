adcad946e786bf611e8abc2e88d5025b56eecdf1
郭磊 <guolei@gdxsoft.com>
2021-06-25 15:09:41 +0800

    web

--- src/main/java/com/gdxsoft/easyweb/websocket/EwaWebSocketContainer.java
+++ src/main/java/com/gdxsoft/easyweb/websocket/EwaWebSocketContainer.java
@@ -14,6 +14,8 @@ import org.slf4j.LoggerFactory;
  */
 public class EwaWebSocketContainer {
 	private static Logger LOGGER = LoggerFactory.getLogger(EwaWebSocketContainer.class);
+	// concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。若要实现服务端与单一客户端通信的话，
+	// 可以使用Map来存放，其中Key可以为用户标识
 	private static ConcurrentHashMap<String, EwaWebSocket> WEB_SOCKETS_MAP = new ConcurrentHashMap<String, EwaWebSocket>();
 
 	/**
