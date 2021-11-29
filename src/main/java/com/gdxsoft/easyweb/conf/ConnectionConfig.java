d9cc2aa9d6275d409524b8ea13df136c36a9a718
Code Guo <guolei@gdxsoft.com>
2021-11-29 11:43:43 +0800

    ewa module

--- src/main/java/com/gdxsoft/easyweb/conf/ConnectionConfig.java
+++ src/main/java/com/gdxsoft/easyweb/conf/ConnectionConfig.java
@@ -3,6 +3,7 @@
  */
 package com.gdxsoft.easyweb.conf;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import com.gdxsoft.easyweb.utils.UFile;
 import com.gdxsoft.easyweb.utils.UXml;
 import com.gdxsoft.easyweb.utils.Utils;
 import com.gdxsoft.easyweb.utils.msnet.MTableStr;
@@ -27,22 +29,23 @@ public class ConnectionConfig {
 	private String _SchemaName;
 	private MTableStr _Pool;
 	private boolean hiddenInDefine; // 在数据库类映射中隐含此数据库连接
+
 	public ConnectionConfig(Node node) {
 		this.setObj(node);
 	}
-	
+
 	private void setObj(Node node) {
 		Element ele = (Element) node;
 		Map<String, String> params = UXml.getElementAttributes(ele, true);
- 
+
 		this._Name = params.get("name") == null ? "" : params.get("name").toLowerCase();
 		this._ConnectionString = params.get("connectionstring");
 		this._Type = params.get("type");
 		this._SchemaName = params.get("schemaname");
 
 		// 2021-04-11 在数据库类映射中隐含此数据库连接
-		this.hiddenInDefine = Utils.cvtBool( params.containsKey("hiddenindefine"));
-		
+		this.hiddenInDefine = Utils.cvtBool(params.containsKey("hiddenindefine"));
+
 		if (StringUtils.isBlank(_Name) || StringUtils.isBlank(_ConnectionString) || StringUtils.isBlank(_Type)
 				|| StringUtils.isBlank(_SchemaName)) {
 			LOGGER.warn("Invalid database cfg -> " + UXml.asXml(node));
@@ -55,8 +58,36 @@ public class ConnectionConfig {
 			Element p = (Element) ele.getElementsByTagName("pool").item(0);
 			for (int i = 0; i < p.getAttributes().getLength(); i++) {
 				Node att = p.getAttributes().item(i);
-				_Pool.add(att.getNodeName(), att.getNodeValue());
+				String name = att.getNodeName().trim();
+				String value = att.getNodeValue().trim();
+				if ("password".equalsIgnoreCase(name) && value.startsWith("file://")) {
+					value = this.getPasswordFromFile(value);
+				}
+				_Pool.add(name, value);
 			}
+		}
+	}
+
+	/**
+	 * Read password from file
+	 * @param value starts with "file://"
+	 * @return the password 
+	 */
+	private String getPasswordFromFile(String value) {
+		if (!value.startsWith("file://")) {
+			return value;
+		}
+
+		String filePath = value.substring(7);
+
+		try {
+			String content = UFile.readFileText(filePath);
+			LOGGER.debug("Read db password from: {}", value);
+			
+			return content.trim();
+		} catch (IOException e) {
+			LOGGER.error("Read password fail from: {} {}", value, e.getLocalizedMessage());
+			return e.getLocalizedMessage();
 		}
 	}
 
@@ -109,8 +140,6 @@ public class ConnectionConfig {
 	public void setSchemaName(String _SchemaName) {
 		this._SchemaName = _SchemaName;
 	}
-
-	
 
 	/**
 	 * @return the _Pool
