9e9a58b6b8c8fec474c7608763509ebbf66c3003
Code Guo <guolei@gdxsoft.com>
2021-12-11 09:45:49 +0800

    added parameters's parse

--- src/main/java/com/gdxsoft/easyweb/script/display/items/ItemEwaConfigItem.java
+++ src/main/java/com/gdxsoft/easyweb/script/display/items/ItemEwaConfigItem.java
@@ -3,6 +3,9 @@
  */
 package com.gdxsoft.easyweb.script.display.items;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.gdxsoft.easyweb.data.DTRow;
 import com.gdxsoft.easyweb.data.DTTable;
 import com.gdxsoft.easyweb.script.PageValueTag;
@@ -17,6 +20,8 @@ import com.gdxsoft.easyweb.script.userConfig.UserXItem;
  * 
  */
 public class ItemEwaConfigItem extends ItemBase {
+	private static Logger LOGGER = LoggerFactory.getLogger(ItemEwaConfigItem.class);
+
 	public String createItemHtml() throws Exception {
 		UserXItem userXItem = super.getUserXItem();
 		SysParameters sysParas = super.getHtmlClass().getSysParas();
@@ -24,38 +29,42 @@ public class ItemEwaConfigItem extends ItemBase {
 		RequestValue rv = new RequestValue(rv1.getRequest(), rv1.getSession());
 
 		if (sysParas.getFrameType().equalsIgnoreCase("listFrame")) {
-			DTTable tb = super.getHtmlClass().getItemValues()
-					.getListFrameTable();
+			DTTable tb = super.getHtmlClass().getItemValues().getListFrameTable();
 			DTRow row = tb.getCurRow();
 			for (int i = 0; i < tb.getColumns().getCount(); i++) {
-				rv.addValue(tb.getColumns().getColumn(i).getName(), row
-						.getCell(i).getValue());
+				rv.addValue(tb.getColumns().getColumn(i).getName(), row.getCell(i).getValue());
 			}
 		}
 		String val;
-		try {
+		String xmlName = userXItem.getSingleValue("DefineFrame", "CallXmlName");
+		xmlName = super.getHtmlClass().getItemValues().replaceParameters(xmlName, false, true);
 
-			String xmlName = userXItem.getSingleValue("DefineFrame",
-					"CallXmlName");
-			String itemName = userXItem.getSingleValue("DefineFrame",
-					"CallItemName");
-			String refParas = userXItem.getSingleValue("DefineFrame",
-					"CallPara");
+		String itemName = userXItem.getSingleValue("DefineFrame", "CallItemName");
 
-			String[] paras = null;
+		String refParas = userXItem.getSingleValue("DefineFrame", "CallPara");
+		refParas = super.getHtmlClass().getItemValues().replaceParameters(refParas, false, true);
 
-			if (refParas.trim().length() > 0) {
-				paras = userXItem.getSingleValue("DefineFrame", "CallPara")
-						.split(",");
+		String[] paras = null;
+		if (refParas.trim().length() > 0) {
+			if (refParas.indexOf("=") > 0) { 
+				// 新方法 name=@user_name&phone=@user_phone
+				this.attachParas(refParas, rv);
+			} else {
+				// 老方法 name,phone
+				paras = refParas.split(",");
 				for (int i = 0; i < paras.length; i++) {
-					if (paras[i].trim().length() > 0) {
-						String paraVal = super.getHtmlClass().getItemValues()
-								.getValue(paras[i], paras[i]);
-						rv.addValue(paras[i], paraVal,
-								PageValueTag.QUERY_STRING);
+					String para = paras[i].trim();
+					if (para.length() == 0) {
+						continue;
 					}
+					// 只有参数名称，没有赋值
+					String paraVal = super.getHtmlClass().getItemValues().getValue(para, para);
+					rv.addValue(paras[i], paraVal, PageValueTag.QUERY_STRING);
 				}
 			}
+		}
+
+		try {
 
 			rv.addValue("EWA_AJAX", "TOP_CNT_BOTTOM");
 			// 调用模式，用于判断使用
@@ -71,15 +80,38 @@ public class ItemEwaConfigItem extends ItemBase {
 			hc.init(rv, super.getResponse());
 			hc.createPageHtml();
 			val = hc.getPageHtml();
-			val = val
-					.replace("@SYS_FRAME_UNID", rv.getString("SYS_FRAME_UNID"));
+			val = val.replace("@SYS_FRAME_UNID", rv.getString("SYS_FRAME_UNID"));
 
 		} catch (Exception e) {
+			LOGGER.error(e.getLocalizedMessage());
 			val = e.getMessage();
 		}
 		String s1 = super.getXItemFrameHtml();
-		s1 = s1.replace(SkinFrame.TAG_VAL, val == null ? "" : val.replace("@",
-				"\1\2$$##GDX~##JZY$$\3\4")); // 替换值
+		s1 = s1.replace(SkinFrame.TAG_VAL, val == null ? "" : val.replace("@", "\1\2$$##GDX~##JZY$$\3\4")); // 替换值
 		return s1.trim();
+	}
+
+	/**
+	 * 初始化的参数放到RV中
+	 * 
+	 * @param paras
+	 */
+	private void attachParas(String paras, RequestValue rv) {
+		if (paras == null || paras.trim().length() == 0) {
+			return;
+		}
+		String[] ps = paras.split("\\&");
+		for (int i = 0; i < ps.length; i++) {
+			String[] pp = ps[i].split("=");
+			if (pp.length == 1 || pp.length > 2) {
+				continue;
+			}
+			String key = pp[0].trim();
+			if (key.length() == 0) {
+				continue;
+			}
+			rv.addValue(key, pp[1], PageValueTag.OTHER);
+		}
+
 	}
 }
