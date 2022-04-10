7639e8396a86e8d2de51559143d3d489696ac9f7
admin <admin@192.168.1.133>
2022-04-10 12:47:51 +0800

    1

--- src/main/java/com/gdxsoft/easyweb/script/ValidCode1.java
+++ src/main/java/com/gdxsoft/easyweb/script/ValidCode1.java
@@ -15,8 +15,8 @@ import java.util.Random;
 
 /**
  * ref http://blog.csdn.net/ruixue0117/article/details/22829557
- * doker: apk add ttf-dejavu
- *		  fc-cache -fv
+ * docker: apk add ttf-dejavu
+ *		   fc-cache -fv
  * @author admin
  *
  */
