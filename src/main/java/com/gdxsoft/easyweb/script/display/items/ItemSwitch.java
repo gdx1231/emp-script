5a9e5db1e54cf712e46bf0218d75926debe2d0b9
Code guo <guolei@gdxsoft.com>
2022-04-24 09:41:48 +0800

    switch value encode

--- src/main/java/com/gdxsoft/easyweb/script/display/items/ItemSwitch.java
+++ src/main/java/com/gdxsoft/easyweb/script/display/items/ItemSwitch.java
@@ -1,21 +1,35 @@
 package com.gdxsoft.easyweb.script.display.items;
 
+import com.gdxsoft.easyweb.script.template.SkinFrame;
+import com.gdxsoft.easyweb.utils.Utils;
+
 /**
  * 用于开关元素的渲染
+ * 
  * @author 郭磊
  */
 public class ItemSwitch extends ItemBase {
-	public String createItemHtml() throws Exception {
-		String html = super.createItemHtml().trim();
+	/**
+	 * 生成对象HTML
+	 */
 
+	public String createItemHtml() throws Exception {
+		String html = super.getXItemFrameHtml();
 		String val = super.getValue();
-		//打开开关的值
+		if (val == null) {
+			val = "";
+		} else {
+			val = val.trim();
+		}
+		// 打开开关的值
 		String swtOnValue = super.getUserXItem().getSingleValue("Switch", "SwtOnValue");
-		if (swtOnValue != null && swtOnValue.length() > 0 && swtOnValue.equalsIgnoreCase(val)) {
+		if (val.length() > 0 && val.equals(swtOnValue == null ? "" : swtOnValue.trim())) {
 			html = html.replace("[checked]", "checked");
 		} else {
 			html = html.replace("[checked]", "");
 		}
+		// 替@
+		html = html.replace(SkinFrame.TAG_VAL, Utils.textToInputValue(val.replace("@", REP_AT_STR)));
 
 		return html;
 	}
