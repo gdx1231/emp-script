27c7b4c5d3bc7fac697e082a0fbe7edaea7199db
Code guo <guolei@gdxsoft.com>
2023-03-09 10:46:39 +0800

    HikariCP Possibly consider using a shorter maxLifetime value.

--- src/main/java/com/gdxsoft/easyweb/datasource/DataHelper.java
+++ src/main/java/com/gdxsoft/easyweb/datasource/DataHelper.java
@@ -50,13 +50,15 @@ public class DataHelper {
 	public Connection getConnection() {
 		return _conn;
 	}
+
 	/**
 	 * 外部设置 数据库连接
+	 * 
 	 * @param connection
 	 */
 	public void setConnection(Connection connection) {
-		 _conn = connection;
-		 this._connected = true;
+		_conn = connection;
+		this._connected = true;
 	}
 
 	/**
@@ -143,6 +145,29 @@ public class DataHelper {
 	 * @throws Exception
 	 */
 	private DataSource createMyDatasourcesHikariCP() throws Exception {
+
+		// connectionTimeout
+		// 此属性控制客户端（即您）等待来自池的连接的最大毫秒数。如果超过此时间而没有可用的连接，则会抛出SQLException。可接受的最低连接超时为250
+		// ms。 默认值：30000（30秒）
+
+		// idleTimeout
+		// 此属性控制允许连接在池中保持空闲状态的最长时间。 仅当minimumIdle定义为小于时，此设置才适用maximumPoolSize。池达到连接后，
+		// 空闲连接将不会退出minimumIdle。连接是否以空闲状态退役，最大变化为+30秒，平均变化为+15秒。在此超时之前，连接永远不会因为闲置而退役。值为0表示永远不会从池中删除空闲连接。最小允许值为10000ms（10秒）。
+		// 默认值：600000（10分钟）
+
+		// maxLifetime
+		// 此属性控制池中连接的最大生存期。使用中的连接永远不会停止使用，只有在关闭连接后才将其删除。在逐个连接的基础上，应用较小的负衰减以避免池中的质量消灭。
+		// 我们强烈建议设置此值，它应该比任何数据库或基础结构施加的连接时间限制短几秒钟。
+		// 值0表示没有最大寿命（无限寿命），当然要遵守该idleTimeout设置。最小允许值为30000ms（30秒）。 默认值：1800000（30分钟）
+
+		// minimumIdle
+		// 此属性控制HikariCP尝试在池中维护的最小空闲连接数。如果空闲连接下降到该值以下，并且池中的总连接数少于maximumPoolSize，则HikariCP将尽最大努力快速而有效地添加其他连接。但是，为了获得最佳性能和对峰值需求的响应能力，我们建议不要设置此值，而应让HikariCP充当固定大小的连接池。
+		// 默认值：与maximumPoolSize相同
+
+		// maximumPoolSize
+		// 此属性控制允许池达到的最大大小，包括空闲和使用中的连接。基本上，此值将确定到数据库后端的最大实际连接数。合理的值最好由您的执行环境确定。当池达到此大小并且没有空闲连接可用时，对getConnection（）的调用将connectionTimeout在超时之前最多阻塞毫秒。请阅读有关池大小的信息。
+		// 默认值：10
+
 		LOGGER.info("Using HikariCP");
 		String driverClassName = _Cfg.getPool().get("driverClassName");
 		MStr errors = new MStr();
@@ -160,17 +185,12 @@ public class DataHelper {
 			LOGGER.error("username not defined");
 			errors.al("username not defined");
 		}
-		/*
-		 * if (StringUtils.isBlank(password)) { LOGGER.error("password not defined");
-		 * errors.al("password not defined"); }
-		 */
 		LOGGER.debug("driverClassName={}, username={}, url={}", driverClassName, username, url);
 		if (errors.length() > 0) {
 			throw new Exception(errors.toString());
 		}
 
 		String password = _Cfg.getPool().get("password");
-
 		String maxActive = _Cfg.getPool().get("maxActive");
 		// String maxIdle = _Cfg.getPool().get("maxIdle");
 
@@ -181,6 +201,7 @@ public class DataHelper {
 		cpds.setUsername(username);
 		cpds.setPassword(password);
 		try {
+			// 允许池达到的最大值
 			int maxActive1 = Integer.parseInt(maxActive);
 			cpds.setMaximumPoolSize(maxActive1);
 		} catch (Exception err) {
@@ -194,6 +215,7 @@ public class DataHelper {
 			String maxWait = _Cfg.getPool().get("maxWait");
 			long maxWaitMs = Long.parseLong(maxWait) * 1000;
 			cpds.setIdleTimeout(maxWaitMs);
+			cpds.setMaxLifetime(maxWaitMs);
 		} catch (Exception err) {
 			// 最大空闲时间,60秒内未使用则连接被丢弃。若为0则永不丢弃。Default: 0
 			cpds.setIdleTimeout(60 * 1000);
@@ -236,7 +258,7 @@ public class DataHelper {
 		if (errors.length() > 0) {
 			throw new Exception(errors.toString());
 		}
-		
+
 		String maxActive = _Cfg.getPool().get("maxActive");
 		// String maxIdle = _Cfg.getPool().get("maxIdle");
 
