6ffa4186f09f175c2579ca58144e6784b63958ff
靳朝鹏 <zhaopeng.jin@gyap.org>
2026-08-06 14:16:28 +0800

    _KEY 规则已去掉

--- src/main/java/com/gdxsoft/easyweb/data/DTTable.java
+++ src/main/java/com/gdxsoft/easyweb/data/DTTable.java
@@ -790,8 +790,7 @@ public class DTTable implements Serializable {
 			return false;
 		}
 		String name = passwordColumn.trim().toUpperCase();
-		if (name.indexOf("PASSWORD") >= 0 || name.endsWith("_PWD") || name.endsWith("_PASS")
-				|| name.endsWith("_KEY")) {
+		if (name.indexOf("PASSWORD") >= 0 || name.endsWith("_PWD") || name.endsWith("_PASS")) {
 			return true;
 		}
 		return false;
